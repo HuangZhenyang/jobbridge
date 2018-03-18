1b0b755a25a2b3256a559c3acb08ecb15b425efd
syunk <syunk@foxmail.com>
2018-03-18 16:58:19 +0800

    公司添加行业id字段

--- src/main/java/com/galigaigai/jobbridge/controller/SignController.java
+++ src/main/java/com/galigaigai/jobbridge/controller/SignController.java
@@ -177,7 +177,8 @@ public class SignController {
                     String name = request.getParameter("name");
                     String phoneNum = request.getParameter("phoneNum");
                     String companyIntroduction = request.getParameter("companyIntroduction");
-                    Company company = new Company(userName, name, mailbox, phoneNum, cryptoPassword, companyIntroduction, "", "e", false);
+                    Integer industryId = Integer.parseInt(request.getParameter("industryId"));
+                    Company company = new Company(userName, name, mailbox, phoneNum, cryptoPassword, companyIntroduction, "",industryId, "e", false);
                     companyService.addCompany(company);
                     break;
             }
