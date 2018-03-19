0e75ab2b78d4de5f84e400361290441a97433a36
HanrAx <hzh00112@qq.com>
2018-03-19 14:15:05 +0800

    修改后端适配为前端渲染的方式

--- src/main/java/com/galigaigai/jobbridge/controller/AdminController.java
+++ src/main/java/com/galigaigai/jobbridge/controller/AdminController.java
@@ -1,13 +1,19 @@
 package com.galigaigai.jobbridge.controller;
 
+import com.galigaigai.jobbridge.model.Company;
 import com.galigaigai.jobbridge.model.Student;
+import com.galigaigai.jobbridge.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by HanrAx on 2018/3/13 0013.
@@ -16,9 +22,12 @@ import java.io.IOException;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private CompanyService companyService;
+
     /*
-   * 管理员添加公司企业界面
-   * */
+       * 管理员添加公司企业界面
+       * */
     @GetMapping(value = "/add_company")
     public String adminAddCompany(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
@@ -39,6 +48,23 @@ public class AdminController {
             response.sendRedirect("/");
         }
         return "addAdmin";
+    }
+
+    /**
+     * 管理员审核公司页面以及列表数据
+     */
+    @GetMapping(value = "/audit_company")
+    public String adminAuditCompany(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if(loginUser == null || !("a".equals(((Student)loginUser).getIdentity()))){
+            response.sendRedirect("/");
+        }
+        List<Company> companyList = companyService.findCompanyAuditOrNot(false);
+        model.addAttribute("companyList",companyList);
+
+
+        return "auditCompany";
     }
 
 }
--- src/main/java/com/galigaigai/jobbridge/service/CompanyService.java
+++ src/main/java/com/galigaigai/jobbridge/service/CompanyService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,5 +30,22 @@ public class CompanyService {
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
+    }
+
+    /**
+     * 查找已审核的公司与没有审核的公司
+     * @param AuditOrNot true为选择已审核过的所有公司，false为选择为审核的公司
+     * @return 根据AuditOrNot的值，返回不同的company List
+     */
+    @Transactional
+    public List<Company> findCompanyAuditOrNot(Boolean AuditOrNot){
+        List<Company> companyList = companyRepository.findAll();
+        List<Company> resultCompanyList  = new ArrayList<>();
+        for (Company company : companyList) {
+            if (company.getAuditing() == AuditOrNot){
+                resultCompanyList.add(company);
+            }
+        }
+        return resultCompanyList;
     }
 }
